2f77f2bf40ea2612e8a6f86ca99304753db47e25
Bersam Karbasion <bersam.k@gmail.com>
2019-06-07 23:29:44 +0430

    Add Basic Admin Configuration for Debugging

--- dominos/orders/admin.py
+++ dominos/orders/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from .models import Order, OrderItem, Pizza, Flavor, Size, Customer
+
 # Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'address')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer')
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('pizza', 'count')
+
+@admin.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('flavor', 'size')
+
+@admin.register(Flavor)
+class FlavorAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('name', )
