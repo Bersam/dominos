b0f50f7d5631176efc05f5518f9f8628589c8526
Bersam Karbasion <bersam.k@gmail.com>
2019-06-08 00:53:35 +0430

    Define a viewset for Order

--- dominos/orders/views.py
+++ dominos/orders/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Order
+from .serializers import OrderSerializer
 
 # Create your views here.
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for listing or retrieving orders.
+    """
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
