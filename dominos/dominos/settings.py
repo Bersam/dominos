42f85f2f934ddd72f43492ba67cdcf57efb09862
Bersam Karbasion <bersam.k@gmail.com>
2019-06-07 22:56:31 +0430

    Change Default Database to Postgresql

--- dominos/dominos/settings.py
+++ dominos/dominos/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'dominos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASS': '',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
