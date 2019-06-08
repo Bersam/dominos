ce1302b014f711d3256001f38f3686ec776bf336
Bersam Karbasion <bersam.k@gmail.com>
2019-06-09 00:24:02 +0430

    Define order_item create method

--- dominos/orders/views.py
+++ dominos/orders/views.py
@@ -3,8 +3,8 @@ from django.utils.translation import ugettext_lazy as _
 from rest_framework import viewsets
 from rest_framework.exceptions import NotFound
 
-from .models import Order, OrderItem
-from .serializers import OrderSerializer, OrderItemSerializer
+from .models import Order, OrderItem, Customer
+from .serializers import OrderSerializer, OrderItemSerializer, CustomerSerializer
 
 
 # Create your views here.
@@ -34,13 +34,23 @@ class OrderItemViewSet(viewsets.ModelViewSet):
 
     def get_object(self):
         orderid = self.kwargs.get("orderid", False)
-        order = Order.objects.get(id=orderid)
         pk = int(self.kwargs.get("pk", False))
 
         try:
+            order = Order.objects.get(id=orderid)
             return OrderItem.objects.filter(order=order)[pk-1]
         except (OrderItem.DoesNotExist, IndexError, AssertionError):
-            raise NotFound(_("OrderItem not found."))
+            raise NotFound(_("Order not found."))
 
+    def perform_create(self, serializer):
+        orderid = self.kwargs.get("orderid", False)
+        pk = int(self.kwargs.get("pk", False))
 
-#TODO Create a View for modifiying Customers
+        try:
+            order = Order.objects.get(id=orderid)
+            order_item = serializer.save()
+            order.order_items.add(order_item)
+            order.save()
+            return OrderItem.objects.filter(order=order)
+        except (Order.DoesNotExist):
+            raise NotFound(_("Order not found."))
